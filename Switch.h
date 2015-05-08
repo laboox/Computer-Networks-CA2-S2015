c7f6915a3ea1771ece54eb28bebfe4e122d5a4ed
aidamsv <moosavifar1993@yahoo.com>
2015-05-08 18:41:22 +0430

    in the name of god :) start CA2 :)

---
+++ Switch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "primary_header.h"
+
+class Switch
+{
+public:
+	Switch(arguments);
+	~Switch();
+
+private:
+	int port;
+};
---
+++ libs/primary_header.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <exception>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <unistd.h>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <time.h> 
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
